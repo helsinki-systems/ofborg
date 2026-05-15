158a5f9ddd84892f4aa103305903d8b5294df910
Simon Hauser <simon.hauser@helsinki-systems.de>
2026-05-15 15:43:37 +0200

    feat: configure hydra_evaluator

--- mprocs/bootstrap-ofborg.sh
+++ mprocs/bootstrap-ofborg.sh
@@ -52,6 +52,17 @@ cat <<EOF > .ofborg-data/local.json
             "virtualhost": "ofborg"
         }
     },
+    "hydra_evaluator": {
+        "rabbitmq": {
+            "host": "localhost:5672",
+            "ssl": false,
+            "username": "ofborg",
+            "password_file": "${DATA_DIR}/.amqp-password",
+            "virtualhost": "ofborg"
+        },
+        "gateway_endpoint": "[::1]:50051",
+        "jobset_id": 7
+    },
     "stats": {
         "listen": "[::1]:9898",
         "rabbitmq": {
